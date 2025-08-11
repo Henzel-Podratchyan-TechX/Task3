08c9c5a592a712e4c951eb2f6c9e15acedfbb1d1
Henzel <henzel.techx@gmail.com>
2025-08-11 17:24:42 +0400

    Added part 1

---
+++ main.js
@@ -0,0 +1,12 @@
+//Part 1
+const firstName = "Henzel";
+const lastName = "Podratchyan";
+const birthYear = 2003;
+let currentYear = 2025;
+let isStudent = true;
+const hobbies = ["Watching movies", "Playing games", "Collecting cards", "Experimenting"]
+const contact = {
+    email: "example@mail.com",
+    phone: "+37498765432",
+    city: "Yerevan"
+}
